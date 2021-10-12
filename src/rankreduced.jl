3ef7a762b8d7ee7257517c16fef6f16a55f7417e
marius <mariusmillea@gmail.com>
2021-10-11 19:48:53 -0700

    rank reduced fix

--- src/rankreduced.jl
+++ src/rankreduced.jl
@@ -11,7 +11,7 @@ end
 ∇θ_logLike(rrprob::RankReducedMPMProblem, x, θ, z) = rrprob.U' * ∇θ_logLike(rrprob.prob, x, modes_to_original_θ(rrprob, θ), z)
 logLike_and_∇z_logLike(rrprob::RankReducedMPMProblem, x, θ, z) = logLike_and_∇z_logLike(rrprob.prob, x, modes_to_original_θ(rrprob, θ), z)
 sample_x_z(rrprob::RankReducedMPMProblem, rng::AbstractRNG, θ) = sample_x_z(rrprob.prob, rng, modes_to_original_θ(rrprob, θ))
-ẑ_at_θ(rrprob::RankReducedMPMProblem, x, θ, z₀) = ẑ_at_θ(rrprob.prob, x, modes_to_original_θ(rrprob, θ), z₀)
+ẑ_at_θ(rrprob::RankReducedMPMProblem, x, θ, z₀; kwargs...) = ẑ_at_θ(rrprob.prob, x, modes_to_original_θ(rrprob, θ), z₀; kwargs...)
 
 modes_to_original_θ(rrprob::RankReducedMPMProblem, m) = rrprob.θ₀ + rrprob.U * m
 
