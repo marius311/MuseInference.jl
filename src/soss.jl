c3a9fdaf3c0b86cbd94630e846ae6d082b7b1ea7
marius <mariusmillea@gmail.com>
2022-05-06 11:47:45 -0700

    fix CI hang, better Soss standardizeθ

--- src/soss.jl
+++ src/soss.jl
@@ -25,10 +25,10 @@ function SossMuseProblem(
     !isempty(x) || error("Model must be conditioned on observed data.")
     sim = rand(model)
     observed_vars = keys(x)
-    hyper_vars = params
-    latent_vars = keys(delete(sim, (observed_vars..., hyper_vars...)))
-    model_for_prior = Soss.likelihood(Soss.Model(model), hyper_vars...)(Soss.argvals(model))
-    xform_z = Soss.xform(model | select(sim, hyper_vars))
+    hyper_vars = (((k, eltype(sim[k])) for k in keys(sim) if k in params)...,)
+    latent_vars = keys(delete(sim, (observed_vars..., first.(hyper_vars)...)))
+    model_for_prior = Soss.likelihood(Soss.Model(model), first.(hyper_vars)...)(Soss.argvals(model))
+    xform_z = Soss.xform(model | select(sim, first.(hyper_vars)))
     xform_θ = Soss.xform(model | select(sim, latent_vars))
     SossMuseProblem(
         autodiff,
@@ -59,29 +59,35 @@ function logPriorθ(prob::SossMuseProblem, θ::AbstractVector, ::Transformedθ)
 end
 
 function ∇θ_logLike(prob::SossMuseProblem, x, z::AbstractVector, θ::ComponentVector, ::UnTransformedθ)
-    like = prob.model | (;_namedtuple(x)..., TV.transform(prob.xform_z, z)...)
+    like = prob.model | (;x..., TV.transform(prob.xform_z, z)...)
     first(AD.gradient(prob.autodiff, θ -> Soss.logdensity(like, _namedtuple(θ)), θ))
 end
 function ∇θ_logLike(prob::SossMuseProblem, x, z::AbstractVector, θ::AbstractVector, ::Transformedθ)
-    like = prob.model | (;_namedtuple(x)..., TV.transform(prob.xform_z, z)...)
+    like = prob.model | (;x..., TV.transform(prob.xform_z, z)...)
     first(AD.gradient(prob.autodiff, θ -> Soss.logdensity(like, _namedtuple(inv_transform_θ(prob, θ))), θ))
 end
 
 
 function logLike_and_∇z_logLike(prob::SossMuseProblem, x, z, θ)
-    first.(AD.value_and_gradient(prob.autodiff, z -> Soss.logdensity(prob.model | (;_namedtuple(x)..., _namedtuple(θ)...), TV.transform(prob.xform_z, z)), z))
+    first.(AD.value_and_gradient(prob.autodiff, z -> Soss.logdensity(prob.model | (;x..., _namedtuple(θ)...), TV.transform(prob.xform_z, z)), z))
 end
 
 function sample_x_z(prob::SossMuseProblem, rng::AbstractRNG, θ)
     sim = Soss.predict(rng, prob.model, _namedtuple(θ))
-    x = ComponentVector(select(sim, prob.observed_vars))
+    x = select(sim, prob.observed_vars)
     z = TV.inverse(prob.xform_z, select(sim, prob.latent_vars))
     (;x, z)
 end
 
-standardizeθ(prob::SossMuseProblem, θ::NamedTuple) = 1f0 * ComponentVector(θ) # ensure at least Float32
-standardizeθ(prob::SossMuseProblem, θ::Number) = 
-    length(prob.hyper_vars) == 1 ? standardizeθ(prob, (;θ)) : error("Invalid θ type for this problem.")
+# all user-provided θ pass through this first so get it right type /
+# order so the user can be lazy
+function standardizeθ(prob::SossMuseProblem, θ::NamedTuple)
+    Set(keys(θ)) == Set(first.(prob.hyper_vars)) || error("Expected θ to have keys: $(first.(prob.hyper_vars)).")
+    ComponentVector((;(k => T.(θ[k]) for (k,T) in prob.hyper_vars)...))
+end
+function standardizeθ(prob::SossMuseProblem, θ::Number)
+    standardizeθ(prob, (;(prob.hyper_vars[1][1] => θ)))
+end
 
 _params_from_θ₀(θ₀::Number) = (:θ,)
 _params_from_θ₀(θ₀) = keys(θ₀)
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 
 using MuseInference, Soss, Turing, Distributions, Random, 
-    Zygote, MeasureTheory, Test, StableRNGs
+    Zygote, MeasureTheory, Test, StableRNGs, LinearAlgebra
 
 ##
 
@@ -12,15 +12,15 @@ rng = StableRNG(0)
 
         Turing.@model function turing_funnel()
             θ ~ Distributions.Normal(0, 3)
-            z ~ Distributions.MvNormal(512, exp(θ/2))
-            x ~ Distributions.MvNormal(z, 1)
+            z ~ Distributions.MvNormal(zeros(512), exp(θ/2) * I)
+            x ~ Distributions.MvNormal(z, I)
         end
 
         @testset "$name" for (name,autodiff) in [
             ("ForwardDiff", MuseInference.ForwardDiffBackend()), 
             ("Zygote", MuseInference.ZygoteBackend())
         ]
-            
+
             (;x) = rand(copy(rng), turing_funnel() | (θ=0,))
             prob = TuringMuseProblem(turing_funnel() | (;x); autodiff)
             MuseInference.check_self_consistency(prob, (θ=1,), has_volume_factor=true, rng=copy(rng))
@@ -37,14 +37,15 @@ rng = StableRNG(0)
 
         soss_funnel = Soss.@model (σ) begin
             θ ~ Distributions.Normal(0, σ)
-            z ~ Distributions.MvNormal(512, exp(θ/2))
-            x ~ Distributions.MvNormal(z, 1)
+            z ~ Distributions.MvNormal(zeros(512), exp(θ/2) * I)
+            x ~ Distributions.MvNormal(z, I)
         end
 
         @testset "$name" for (name,autodiff) in [
             ("ForwardDiff", MuseInference.ForwardDiffBackend()), 
             ("Zygote", MuseInference.ZygoteBackend())
         ]
+
             (;x) = Soss.predict(copy(rng), soss_funnel(3), θ=0)
             prob = SossMuseProblem(soss_funnel(3) | (;x); autodiff)
             MuseInference.check_self_consistency(prob, (θ=1,), has_volume_factor=false, rng=copy(rng))
@@ -69,6 +70,7 @@ rng = StableRNG(0)
             ("ForwardDiff", MuseInference.ForwardDiffBackend()), 
             # ("Zygote", MuseInference.ZygoteBackend()) # broken
         ]
+
             (;x) = Soss.predict(copy(rng), soss_funnel(3), θ=0)
             prob = SossMuseProblem(soss_funnel(3) | (;x); autodiff)
             MuseInference.check_self_consistency(prob, (θ=1,), has_volume_factor=false, rng=copy(rng))
